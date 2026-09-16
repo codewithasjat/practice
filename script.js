4d0b40888acd1b849776d887f24c2e3b7e785f48
asjat <asjatbagwan@gmail.com>
2026-09-16 21:46:23 +0530

    merge sort

--- script.js
+++ script.js
@@ -193,18 +193,99 @@
 // console.log(arr3);
  
 
- let num =7 
- for(let i=0 ; i<= num ;i++){
-   let str=""
-  for(let j=0 ; j<=i; j++){
-     
-   str += String.fromCharCode(65 +j)
+//  let arr =[10, 5, 8, 20, 15];
+// function findUnique(arr){
+//    let largest =arr[0]
+//    let secodn = 9
+//    for(let i=0 ; i<=arr.length-1 ; i++){
+//       if(arr[i]<largest){
+//          secodn = largest
+//          largest = arr[i]
+//       }
+//       else if(arr[i]<secodn && arr[i]>largest){
+//          secodn = arr[i]
+//       }
+//    }
+//   return secodn
+// }
+//  console.log(findUnique(arr ));
 
-  }
-   console.log(str);
+let arr =[4,2,3,5,1,2,6]
+//  function mergeSort(arr,l,h){
+//    if(l>=h) return;
+//    let mid = Math.floor((l+h)/2)
+//    mergeSort(arr,l,mid)
+//    mergeSort(arr,mid+1,h)
+//    merge(arr,l,mid,h)
+
+//  }
+// function merge(arr,l,mid,h){
+//    let left =l
+//    let temp=[]
+//    let right = mid+1
+//    while (left<=mid && right<=h){
+//    if(arr[left]<=arr[right]){
+//       temp.push(arr[left])
+//       left++
+//    }
+//    else{
+//       temp.push(arr[right])
+//       right++
+//    }}
+
+//    while(left<=mid){
+//       temp.push(arr[left])
+//       left++
+//    }
+//    while(right<=h){
+//       temp.push(arr[right])
+//       right++
+//    }
+//    for(let i=l ; i<=h ;i++){
+//       arr[i]=temp[i-l]
+//    }
+// }
+
+//  mergeSort(arr,0,arr.length-1)
+//  console.log(arr);
+
+
+ function mergeSort(arr,l,h){
+   if(l>=h) return ;
+   let mid = Math.floor((l+h)/2)
+   mergeSort(arr,l,mid)
+   mergeSort(arr,mid+1,h)
+   merge(arr,l,mid,h)
+
  }
-
-
+  function merge(arr,l,mid,h){
+   let left =l
+   let right = mid+1
+   let temp =[]
+   while(left<=mid && right <=h){
+      if(arr[left]<= arr[right]){
+         temp.push(arr[left])
+         left++
+      }
+      else{
+         temp.push(arr[right])
+         right++
+      }
+   }
+  while(left<=mid){
+   temp.push(arr[left])
+   left++
+  }
+  while(right<=h){
+   temp.push(arr[right])
+   right++
+  }
+for(let i=l ; i<=h ; i++ ){
+   arr[i]=temp[i-l]
+}
+ }
+ mergeSort(arr,0,arr.length-1)
+ console.log(arr);
 
 
 
